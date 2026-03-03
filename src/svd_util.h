a9623c8a73eefdaa1b6661f98b58e44ca70639ba
Muqeet Salam <muqeetsalam168@gmail.com>
2026-03-03 06:17:25 +0000

    testing

--- src/svd_util.h
+++ src/svd_util.h
@@ -1,5 +1,4 @@
 // SPDX-FileCopyrightText: 2007-2020 pancake <pancake@nopcode.org>
-// SPDX-FileCopyrightText: 2026 Muqeet Salam <muqeetsalam168@gmail.com>
 // SPDX-License-Identifier: LGPL-3.0-only
 
 // Internal utility header for rz-svd standalone compilation.
